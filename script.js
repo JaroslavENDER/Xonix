abc2895a103964dead59e4668fe4e0c34d24cfa2
Ender <jaroslav.ender@gmail.com>
2018-05-12 10:10:25 +0700

    Added gameObjects[] and conix

--- script.js
+++ script.js
@@ -2,9 +2,11 @@ var xonixGame = (function () {
     var canvas;
     var resources;
     var gameObjects;
+    var xonix;
     var width = 640;
     var height = 360
     var level = 1;
+    var directions = { up: 1, right: 2, bottom: 3, left: 4 };
 
     var init = function () {
         canvas = document.getElementById("canvas").getContext("2d");
@@ -14,16 +16,61 @@ var xonixGame = (function () {
         resources.background = new Image();
         resources.background.src = "images/level" + level + ".jpg";
     }
-    var createGameObjects = function (){
-
+    var createGameObjects = function () {
+        gameObjects = [];
+        xonix = (function () {
+            var x = 300;
+            var y = 0;
+            var [width, height] = [10, 10];
+            var color = "#000";
+            var maxVelocity = 2;
+            var [currentOffsetX, currentOffsetY] = [0, 0];
+            return {
+                render: function (canvas) {
+                    canvas.fillRect(x, y, width, height);
+                },
+                setDirection: function (direction) {
+                    switch (direction) {
+                        case directions.up:
+                            currentOffsetY = -maxVelocity;
+                            break;
+                        case directions.bottom:
+                            currentOffsetY = maxVelocity;
+                            break;
+                        case directions.left:
+                            currentOffsetX = -maxVelocity;
+                            break;
+                        case directions.right:
+                            currentOffsetX = maxVelocity;
+                            break;
+                    }
+                },
+                stop: function () {
+                    currentOffsetX = currentOffsetY = 0;
+                },
+                update: function () {
+                    x += currentOffsetX;
+                    y += currentOffsetY;
+                }
+            }
+        })();
+        gameObjects.push(xonix);
     }
     var run = function () {
-        canvas.clearRect(0, 0, width, height);
-        render();
-        window.requestAnimationFrame(function () { run(); });
+        update();
+        render(run);
     }
-    var render = function () {
+    var update = function () {
+        for (var key in gameObjects) {
+            gameObjects[key].update();
+        }
+    }
+    var render = function (callback) {
+        canvas.clearRect(0, 0, width, height);
         canvas.drawImage(resources.background, 0, 0 - Math.abs(resources.background.height - height) / 2);
+        for (var key in gameObjects)
+            gameObjects[key].render(canvas);
+        window.requestAnimationFrame(function () { callback(); });
     }
 
     return {
@@ -39,6 +86,6 @@ var xonixGame = (function () {
             return canvas;
         }
     }
-}());
+})();
 
 window.onload = xonixGame.start;
